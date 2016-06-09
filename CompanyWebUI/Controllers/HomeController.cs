1b74449eae4677a9efc302da076aceee6130ad79
Jimolle <jimolofsamuel@gmail.com>
2016-06-09 21:22:32 +0200

    WCF-service tcp-binding complete

--- CompanyWebUI/Controllers/HomeController.cs
+++ CompanyWebUI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CompanyWebUI.ServiceReference;
 
 namespace CompanyWebUI.Controllers
 {
@@ -10,7 +11,22 @@ namespace CompanyWebUI.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            List<Product> products;
+            string info;
+            string info2;
+
+            using (var proxy = new ProductServiceClient())
+            {
+                info = proxy.Endpoint.Binding.Name;
+                info2 = proxy.Endpoint.Address.Uri.ToString();
+
+                products = proxy.GetAllProducts().ToList();
+            }
+            ViewBag.Info = info;
+            ViewBag.Info2 = info2;
+
+
+            return View(products);
         }
 
         public ActionResult About()
